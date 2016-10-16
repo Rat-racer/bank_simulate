31a32cdc318490215edc456212664f083c8980de
vincent <apple@appledemacbook-air-3.local>
2016-10-16 19:12:08 +0800

    Initial Commit

---
+++ bank_simulate/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  bank_simulate
+//
+//  Created by Apple on 16/10/16.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
